b8c06e80eddff913750c42882f1bd72d41550985
arif kurniawan <arifkurniawan98@outlook.com>
2023-10-09 17:34:30 +0700

    feat: add model

--- internal/model/produk.go
+++ internal/model/produk.go
@@ -1,1 +1,31 @@
 package model
+
+import "time"
+
+type ProductType string
+type Model string
+
+const (
+	ProductTypeSaham           ProductType = "Saham"
+	ProductTypePasaruang       ProductType = "Pasar Uang"
+	ProductTypePendapatanTetap ProductType = "Pendapatan Tetap"
+)
+
+const (
+	ModelKonvensional Model = "Konvensional"
+	ModelSyariah      Model = "Syariah"
+)
+
+type Product struct {
+	ID             int         `json:"id"`
+	Name           string      `json:"name"`
+	Type           ProductType `json:"type"`
+	ModelProduct   Model       `json:"model_product"`
+	Price          float64     `json:"price"`
+	Profit         float64     `json:"profit"`
+	ProductManager string      `json:"product_manager"`
+	ReleaseDate    time.Time   `json:"release_date"`
+	CreatedAt      time.Time   `json:"created_at" db:"created_at"`
+	UpdatedAt      time.Time   `json:"updated_at" db:"updated_at"`
+	DeletedAt      *time.Time  `json:"deleted_at,omitempty" db:"deleted_at"`
+}
--- internal/model/transaction.go
+++ internal/model/transaction.go
@@ -1,1 +1,23 @@
 package model
+
+import "time"
+
+type StatusTransaction string
+
+const (
+	StatusTransactionSuccess StatusTransaction = "Success"
+	StatusTransactionFailed  StatusTransaction = "Failed"
+)
+
+type Transaction struct {
+	ID         int        `json:"id"`
+	UserID     int        `json:"user_id"`
+	ProductID  int        `json:"product_id"`
+	CampaignID int        `json:"campaign_id"`
+	Total      int        `json:"total"`
+	Amount     float64    `json:"amount"`
+	Status     float64    `json:"status"`
+	CreatedAt  time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time  `json:"updated_at" db:"updated_at"`
+	DeletedAt  *time.Time `json:"deleted_at,omitempty" db:"deleted_at"`
+}
